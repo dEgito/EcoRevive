47df6ece6aa87ded76fc6b759936410f14146781
Danielly Egito <75611712+dEgito@users.noreply.github.com>
2023-07-13 14:36:48 -0300

    feat: adiciona seções de "procedimento", "benefícios" e "o que é"

---
+++ eco-revive/src/components/TopBlueWave/index.js
@@ -0,0 +1,10 @@
+import WaveImg from "../../assets/Top_blue_wave.png"
+import { Wave } from "./style";
+
+function TopBlueWave() {
+  return (
+    <Wave src={WaveImg}/>
+  )
+}
+
+export default TopBlueWave;
---
+++ eco-revive/src/components/bottomWhiteWave/index.js
@@ -0,0 +1,10 @@
+import WaveImg from "../../assets/Bottom_white_wave.png"
+import { Wave } from "./style";
+
+function BottomWhiteWave() {
+  return (
+    <Wave src={WaveImg}/>
+  )
+}
+
+export default BottomWhiteWave;
