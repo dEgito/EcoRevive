a395ed1a39258f829e5c43b81a1a63e493c1ae28
akyssaeduarda <akyssaeduarda.dev@gmail.com>
2023-07-28 18:15:51 -0300

    feat: tela de erro

--- eco-revive/src/routes.js
+++ eco-revive/src/routes.js
@@ -5,10 +5,11 @@ import ContactUs from "./pages/contactUs/contact";
 import GuidePage from "./pages/home/home";
 import AboutUs from "./pages/aboutUs/index";
 import CollectionLocations from "./pages/collectionLocationsPage/index";
-import Faqs from "./pages/faqs/faqs"
+import Faqs from "./pages/faqs/faqs";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import WaitPage from "./pages/waitPage/wait";
+import Error from "./pages/error/error";
 
 export const routes = () => {
   return (
@@ -20,9 +21,10 @@ export const routes = () => {
         <Route path="/locais" element={<CollectionLocations />} />
         <Route path="/agendamento" element={<DisposalSchedule />} />
         <Route path="login" element={<Login />} />
-        <Route path="cadastro" element={<Register />}/>
-        <Route path="duvidas" element={<Faqs />}/>
+        <Route path="cadastro" element={<Register />} />
+        <Route path="duvidas" element={<Faqs />} />
         <Route path="inicio" element={<WaitPage />} />
+        <Route path="/erro" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
