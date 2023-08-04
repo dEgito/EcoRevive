c1d98e2c52d230be2c4960b487f73cf7b2a83479
Emilly Maria Araújo da Silva <emillymaria93@gmail.com>
2023-08-04 16:00:39 -0300

    fix/ correção imagem

--- eco-revive/src/pages/register/style.js
+++ eco-revive/src/pages/register/style.js
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import Background from "../../assets/Login_Background.png"
+import Background from "../../assets/Register_users.jpg"
 
 export const Container = styled.div`
   display: flex;
