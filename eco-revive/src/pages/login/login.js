4edf36bf2628a022408478ce6192f2893da6f15c
Danielly Egito <75611712+dEgito@users.noreply.github.com>
2023-08-04 13:51:59 -0300

    fix: corrige inputs do login

--- eco-revive/src/pages/login/login.js
+++ eco-revive/src/pages/login/login.js
@@ -9,36 +9,34 @@ import { api } from "../../Services/Api";
 
 // Components
 import Button from "../../components/button/index";
-import Input from "../../components/input/index";
 import DefaultNavbar from "../../components/defaultNavbar/navbar";
 
 // Styles
-import { ButtonBox, Container, Content } from "./style";
+import { ButtonBox, Container, Content, InputContent } from "./style";
 
 // Assets
 import Logo from "../../assets/Logo320.png";
 import { set } from "react-hook-form";
 
-
 function Login() {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState("")
-  const history = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useNavigate();
 
   function handleSubmit(e) {
-    e.preventDefault()
-    api.post('/login', {
-      'user_email': email,
-      'user_password': password
-    
-    }).then((response)=>{
-      localStorage.setItem('token', response.data.token)
-      history("/home")
-
-    }).catch((error)=>{
-      return alert("Erro inesperado")
-    })
+    e.preventDefault();
+    api
+      .post("/login", {
+        user_email: email,
+        user_password: password,
+      })
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        history("/home");
+      })
+      .catch((error) => {
+        return alert("Erro inesperado");
+      });
   }
 
   return (
@@ -50,24 +48,26 @@ function Login() {
         <Content>
           <h1>Login</h1>
           <form onSubmit={handleSubmit}>
-            <input              
-              type="text"
-              label="Email"
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              label="Senha"
-              name="password"
-              onChange = {(e) => setPassword(e.target.value)}
-            />
+            <InputContent category={"secundary"}>
+              <input
+                type="text"
+                label="Email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </InputContent>
+            <InputContent category={"secundary"}>
+              <input
+                type="password"
+                label="Senha"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </InputContent>
 
-            
             <Button category={"primary"} type={"submit"}>
               Entrar
             </Button>
-            
           </form>
 
           <ButtonBox>
